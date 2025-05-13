bf4e3c7925338d3f9bfd017da1c00826fff268cd
Salma Hernández <157586002+salmahshs@users.noreply.github.com>
2025-05-13 11:30:42 +0200

    arreglos

--- app/src/main/java/com/example/myapplication/activities/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/activities/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.example.myapplication.utils.Preferencias
@@ -21,13 +22,16 @@ class MainActivity : AppCompatActivity() {
         // Postergar la transición a la actividad de inicio de sesión después de 1500 milisegundos (1.5 segundos)
         Handler(Looper.getMainLooper()).postDelayed({
             val token = Preferencias.obtenerValorString("token", "")
+            val nombreUsuario = Preferencias.obtenerValorString("nombreUsuario", "")
+            Log.e("MainActivity", "token: ${token}")
+            Log.e("MainActivity", "user: ${nombreUsuario}")
 
-            /*val nextActivity = if (token != null) {
+            val nextActivity = if (!token.isNullOrEmpty()) {
                 Home::class.java
             } else {
                 Inicio::class.java
-            }*/
-            val nextActivity =  Inicio::class.java
+            }
+            //val nextActivity =  Inicio::class.java
 
             val intent = Intent(this, nextActivity)
             startActivity(intent)
